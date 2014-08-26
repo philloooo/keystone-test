00f8c249aab3c394c501c6016a7da57cd1fcecd0
Yajing Tang <yajing@wireless-s1-so-150-55-2.uchicago.edu>
2014-08-26 14:08:59 -0500

    Change it to decorator

--- test.py
+++ test.py
@@ -3,29 +3,42 @@ from keystoneclient.v2_0 import client
 from flask import request
 app=Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
-def auth():
-    print request.form
-    if "auth_url" in request.form:        
-        auth_url=request.form['auth_url']
-        if "token" in request.form:
-            try:
-                client.Client(token=request.form['token'], auth_url)
-                return 'succeed'
-            except: 
-                return "error authentication"
-        elif all (k in request.form for k in ("username", "password","tenant_name")):
-            username=request.form['username']
-            password=request.form['password']
-            tenant_name=request.form['tenant_name']
-            try:
-                client.Client(username=username, password=password,tenant_name=tenant_name,
-                    auth_url)
-                return "succeed"
-            except:
-                return "error authorization"
-    return 'need authorization' 
+def auth(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        print request.form
+        if "auth_url" in request.form:        
+            auth_url=request.form['auth_url']
+            if "token" in request.form:
+                try:
+                    client.Client(token=request.form['token'], auth_url)
+                    return f(*args, **kwargs)
+                except: 
+                    return "error authentication"
+            elif all (k in request.form for k in ("username", "password")):
+                username=request.form['username']
+                password=request.form['password']
+                try:
+                    if "tenant_name" in request.form:
+                        tenant_name=request.form['tenant_name']
+                        client.Client(username=username, password=password,tenant_name=tenant_name,
+                            auth_url)
+                    elif "tenant_id" in request.form:
+                        tenant_id=request.form['tenant_id']
+                        client.Client(username=username, password=password,tenant_id=tenant_id,
+                        auth_url)
+                    else:
+                        client.Client(username=username, password=password,auth_url)
+                    return f(*args, **kwargs)
 
+                except:
+                    return "error authorization"
+    return decorated_function
+
+@app.route('/',methods=['GET','POST'])
+@auth
+def login():
+    return "true"
 
 if __name__ == "__main__":
     app.debug = True    
