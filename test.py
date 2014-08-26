443e0972882c22d5e9eae7a04d697eecdf052836
Yajing Tang <yajing@wireless-s1-so-150-55-2.uchicago.edu>
2014-08-26 13:45:09 -0500

    Using keystoneclient to do the authentication

--- test.py
+++ test.py
@@ -1,41 +1,31 @@
 from flask import Flask
 from keystoneclient.v2_0 import client
-import json
 from flask import request
-import requests
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
-def login():
+def auth():
     print request.form
     if "auth_url" in request.form:        
-    if "token" in request.form:
-        try:
-            client.Client(token=request.form['token'], auth_url)
-    if all (k in request.form for k in ("username", "password","auth_url")):
-        
-        username=request.form['username']
-        password=request.form['password']
         auth_url=request.form['auth_url']
-        auth_data={"auth": {"identity": {
-            "methods": ["password"],
-            "password": {
-                "user": {
-                    "name":username,
-                    "domain":{"id": "default"}, 
-                    "password":password}}}}}
-        headers= {'content-type': 'application/json'}
-        r=requests.post(auth_url,data=json.dumps(auth_data),headers=headers)
-        print r.text
-        print r.json
-        return "succeed"
-    else:
-        print 'need authorization'
-        return 'need authorization' 
+        if "token" in request.form:
+            try:
+                client.Client(token=request.form['token'], auth_url)
+                return 'succeed'
+            except: 
+                return "error authentication"
+        elif all (k in request.form for k in ("username", "password","tenant_name")):
+            username=request.form['username']
+            password=request.form['password']
+            tenant_name=request.form['tenant_name']
+            try:
+                client.Client(username=username, password=password,tenant_name=tenant_name,
+                    auth_url)
+                return "succeed"
+            except:
+                return "error authorization"
+    return 'need authorization' 
 
-@app.route('/hello')
-def hello():
-    return "hello"
 
 if __name__ == "__main__":
     app.debug = True    
